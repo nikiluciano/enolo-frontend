4888f5b66048d3f1ff52f2583fea642d652e4e7d
Rindi00 <v.grassi@studenti.unimol.it>
2022-04-11 23:18:55 +0200

    feat: defined menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private router:Router) {}
+
+  button1(){
+
+  }
+
+  button2(){
+
+  }
+
+  button3(){
+    this.router.navigate(["./carichi"])
+  }
+
+  button4(){
+
+  }
+  
+  button5(){
+
+   }
+
 }
