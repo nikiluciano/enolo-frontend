4b6f437312b4165534b4bbec3f7d65dd29cd2f62
Antonio <64419394+antonioiacampo@users.noreply.github.com>
2022-05-03 15:40:44 +0200

    feature: added html of forgot pw page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ForgotPwPipe } from './forgot-pw.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ForgotPwPipe],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
---
+++ src/app/forgot-pw/forgot-pw.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forgot-pw',
+  templateUrl: './forgot-pw.page.html',
+  styleUrls: ['./forgot-pw.page.scss'],
+})
+export class ForgotPwPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
