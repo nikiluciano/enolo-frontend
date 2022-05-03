9db3267a2ddaf781b78d2d3eefb9e46fccd134b2
Antonio <64419394+antonioiacampo@users.noreply.github.com>
2022-05-03 15:32:54 +0200

    feat: added checks in sign-up form

--- src/app/sign-up/sign-up.page.ts
+++ src/app/sign-up/sign-up.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.page.html',
@@ -11,6 +11,42 @@ export class SignUpPage implements OnInit {
   pass: string;
   telephone: string;
 
+  
+  SignUpForm: FormGroup;
+
+  error_messages = {
+
+    'password': [
+      { type: 'required', message: 'password is required.' },
+      { type: 'minlength', message: 'password length.' },
+      { type: 'maxlength', message: 'password length.' }
+    ],
+    'confirmpassword': [
+      { type: 'required', message: 'password is required.' },
+      { type: 'minlength', message: 'password length.' },
+      { type: 'maxlength', message: 'password length.' },
+    ],
+  }
+
+  constructor(
+    public formBuilder: FormBuilder
+  ) {
+    this.SignUpForm = this.formBuilder.group({
+
+      password: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(30)
+      ])),
+      confirmpassword: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(30)
+      ])),
+    }, { 
+      validators: this.password.bind(this)
+    });
+  }
   ngOnInit() {
   }
   getCredentials() {
@@ -27,6 +63,16 @@ export class SignUpPage implements OnInit {
     this.showPwd = !this.showPwd;
     this.pwdIcon = this.showPwd ? "eye-off-outline" : "eye-outline";
   }
-
+  password(formGroup: FormGroup) {
+    const { value: password } = formGroup.get('password');
+    const { value: confirmPassword } = formGroup.get('confirmpassword');
+    return password === confirmPassword ? null : { passwordNotMatch: true };
+  }
 
 }
+
+
+ 
+
+
+ 
