57ee89e80edda640dd0d9bd031e15add3fbf04e6
Antonio <64419394+antonioiacampo@users.noreply.github.com>
2022-05-04 17:36:12 +0200

    feat: add module.ts password check

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { ForgotPwPipe } from './forgot-pw.pipe';
+
 
 @NgModule({
-  declarations: [AppComponent, ForgotPwPipe],
+  declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
