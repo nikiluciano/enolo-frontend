42c5ddc5e3c70b8ea88a8d2b97d6e06d0821486b
Luigi <luigi@MacBook-Pro-di-Luigi.local>
2022-05-18 21:54:25 +0200

    refactor:modificato posizioni label e modica css

--- src/app/user-account/user-account.page.ts
+++ src/app/user-account/user-account.page.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserAccountPage implements OnInit {
 
+
+  
+  postData = {
+    email: 'nome a caso',
+    nome: 'Nome_user',
+    cognome:'Cognome_user',
+    username:'prova',
+    telefono:'123 4567890',
+    indirizzo:'prova',
+    password:'prova'
+  }
+
+
   constructor() { }
 
   ngOnInit() {
   }
+  pwdIcon = "eye-outline"; 
+  showPwd = false; 
+ 
+  togglePwd() { 
+    this.showPwd = !this.showPwd; 
+    this.pwdIcon = this.showPwd ? "eye-off-outline" : "eye-outline"; 
+  }  
 
 }
