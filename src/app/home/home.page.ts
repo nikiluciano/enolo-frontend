7c8f6c1799612e22afc76a29b37364bb629c2240
Rindi00 <v.grassi@studenti.unimol.it>
2022-05-25 17:23:31 +0200

    feat:add user-account button, remove click function in app-component html,add api warehouse working

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { IonChip } from '@ionic/angular';
 import { WarehouseService } from '../services/warehouse.service';
+import { Storage } from '@capacitor/storage';
+
+
 
 @Component({
   selector: 'app-home',
@@ -8,8 +11,9 @@ import { WarehouseService } from '../services/warehouse.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
+  [x: string]: any;
 
-  constructor(public warehouseservice:WarehouseService) {}
+  constructor(public warehouseService: WarehouseService) { }
 
   Contatti = [
     { id: 1, name: 'mario', carico: 'deraspatura', peso: 1000 },
@@ -18,17 +22,43 @@ export class HomePage implements OnInit {
     { id: 4, name: 'simone', carico: 'vinatura', peso: 1100 }
   ]
 
-  warehouse = [];
-  
-  ngOnInit() {
+  warehouse: any;
+  contentLoaded = false;
+  totalBottles=0;
 
+
+  ngOnInit() {
+    this.getWarehouse()
   }
 
-doRefresh(event){
-  setTimeout(()=>{
-    console.log("piju ngul")
-    event.target.complete()
-  }, 2000);
+  doRefresh(event) {
+    setTimeout(()=>{
+      console.log("piju ngul")
+      event.target.complete()
+    }, 2000);
+    this.getWarehouse()
+  }
+
+  
+
+  async getWarehouse() {
+    await this.warehouseService.getWarehouse().then(
+      (res: any) => {
+        console.log("arrivato qui")
+        if (res) {
+          this.contentLoaded=true
+          this.warehouse = res
+          console.log("magazzino" + JSON.stringify(this.warehouse));
+
+        } else {
+          console.log("errore");
+
+        }
+      })
+
+    }
+
 }
-}
+
+
 
