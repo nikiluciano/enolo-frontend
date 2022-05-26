262d54cc3c144f618e75a0652bc1a95a9bb3806a
unknown <manuelgrimani38@gmail.com>
2022-05-26 17:07:01 +0200

    feat: load design

--- src/app/load/load.page.ts
+++ src/app/load/load.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { Platform, LoadingController } from '@ionic/angular';
-
+import { LoadingController } from '@ionic/angular';
+import { ConfermentService } from '../services/conferment.service';
 @Component({
   selector: 'app-load',
   templateUrl: './load.page.html',
@@ -8,30 +8,34 @@ import { Platform, LoadingController } from '@ionic/angular';
 })
 export class LoadPage implements OnInit {
 
- load= [];
+  postData: any;
 
-  constructor(public platform: Platform, public loadingController: LoadingController) {
-    this.platform.ready().then(() => {
-      this.loadingController.create({
-        message: "loading..."
-      }).then((loadingElement) => {
-        loadingElement.present();
+  constructor(public confermentService: ConfermentService,
+    public loadingController: LoadingController) { }
 
-        var ref = this;
-        setTimeout(function () {
-          ref.loadingController.dismiss();
-        }, 2000)
-      })
-    }
-    )
-
-
+  ngOnInit() {
+    this.getAllConferment();
   }
 
 
 
+  doRefresh(event) {
+    setTimeout(() => {
+      console.log("prova")
+      event.target.complete()
+    }, 2000);
+     this.getAllConferment();
+  }
 
-  ngOnInit() { }
-
-
+  getAllConferment() {
+    this.confermentService.getAllConferments().then(
+      (res: any) => {
+        if (res) {
+          this.postData = res
+        } else {
+          console.log("errore");
+        }
+      }
+    )
+  }
 }
