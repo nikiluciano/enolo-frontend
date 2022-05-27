59fdd64b22af13a11a9654aa47ca1c7209a2afc0
nikiluciano <nikiluciano.nl@gmail.com>
2022-05-27 17:52:02 +0200

    fix: build for ios now working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -43,6 +43,7 @@ const routes: Routes = [
     path: 'insert-conferment',
     loadChildren: () => import('./insert-conferment/insert-conferment.module').then( m => m.InsertConfermentPageModule)
   },
+    {
     path: 'user-account',
     loadChildren: () => import('./user-account/user-account.module').then( m => m.UserAccountPageModule)
   }
