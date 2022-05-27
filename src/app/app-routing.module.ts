ec0346301c223c2124a94390b4e7dcba27b0d02a
Rindi00 <v.grassi@studenti.unimol.it>
2022-05-27 17:49:25 +0200

    feat:added api cosumption, add allert on warehouse status

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,10 +11,10 @@ const routes: Routes = [
     redirectTo: 'login',
     pathMatch: 'full'
   },
-  {
-    path: 'login',
+ {
+   path: 'login',
     loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
-  },
+  }, 
   {
     path: 'sign-up',
     loadChildren: () => import('./sign-up/sign-up.module').then(m => m.SignUpPageModule)
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { IonChip } from '@ionic/angular';
 import { WarehouseService } from '../services/warehouse.service';
 import { Storage } from '@capacitor/storage';
-
+import { ConfermentService } from '../services/conferment.service';
+import { Style } from '@capacitor/status-bar';
 
 
 @Component({
@@ -13,40 +14,35 @@ import { Storage } from '@capacitor/storage';
 export class HomePage implements OnInit {
   [x: string]: any;
 
-  constructor(public warehouseService: WarehouseService) { }
-
-  Contatti = [
-    { id: 1, name: 'mario', carico: 'deraspatura', peso: 1000 },
-    { id: 2, name: 'benzema', carico: 'deraspatura', peso: 1306 },
-    { id: 3, name: 'marra', carico: 'vinatura', peso: 1067 },
-    { id: 4, name: 'simone', carico: 'vinatura', peso: 1100 }
-  ]
+  constructor(public warehouseService: WarehouseService,
+    public confermentService: ConfermentService) { }
 
   warehouse: any;
   contentLoaded = false;
-  totalBottles=0;
-
+  pandingConferments: any;
+  progress:number;
+  
 
   ngOnInit() {
     this.getWarehouse()
+    this.getPandingConferments()
+    this.progress;
   }
 
   doRefresh(event) {
-    setTimeout(()=>{
-      console.log("piju ngul")
+    this.getWarehouse()
+    this.getPandingConferments()
+    setTimeout(() => {
       event.target.complete()
     }, 2000);
-    this.getWarehouse()
-  }
 
-  
+  }
 
   async getWarehouse() {
     await this.warehouseService.getWarehouse().then(
       (res: any) => {
-        console.log("arrivato qui")
         if (res) {
-          this.contentLoaded=true
+          this.contentLoaded = true
           this.warehouse = res
           console.log("magazzino" + JSON.stringify(this.warehouse));
 
@@ -55,10 +51,59 @@ export class HomePage implements OnInit {
 
         }
       })
+  }
 
+  async getPandingConferments() {
+    await this.confermentService.getPandingConferments().then(
+      (res: any) => {
+        if (res) {
+          this.contentLoaded = true
+          this.pandingConferments = res
+          this.findTheCurrentProcess()
+          console.log("conferimenti" + JSON.stringify(this.pandingConferments));
+        }
+        else {
+          console.log("errore");
+        }
+      }
+    )
+  }
+  findTheCurrentProcess() {
+    for (let i = 0; i < this.pandingConferments.length; i++) {
+      if (this.pandingConferments[i].status == "READY") {
+        this.pandingConferments[i].current_process = "Pronto"
+      } else if (this.pandingConferments[i].status == "DELIVERED")
+        this.pandingConferments[i].current_process = "Non iniziato"
+      else if (this.pandingConferments[i].status == null)
+        this.pandingConferments[i].current_process = "Non iniziato"
+      else {
+        switch (this.pandingConferments[i].current_process) {
+          case "wine_pressing_process":
+            this.pandingConferments.current_process = "Pigiatura"
+        }
+        switch (this.pandingConferments[i].current_process) {
+          case "destemming_process":
+            this.pandingConferments[i].current_process = "Diraspatura"
+        }
+        switch (this.pandingConferments[i].current_process) {
+          case "winemaking_process":
+            this.pandingConferments[i].current_process = "Vinificazione"
+        }
+        switch (this.pandingConferments[i].current_process) {
+          case "racking_process":
+            this.pandingConferments[i].current_process = "Svinatura"
+        }
+        switch (this.pandingConferments[i].current_process) {
+          case "refinement_process":
+            this.pandingConferments[i].current_process = "Affinamento"
+        }
+        switch (this.pandingConferments[i].current_process) {
+          case "bottling_process":
+            this.pandingConferments[i].current_process = "Imbottigliamento"
+            this.progress=70;
+        }
+
+      }
     }
-
+  }
 }
-
-
-
