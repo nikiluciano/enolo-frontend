29cfc1c13974f3ecbb2044c3c88584e4ef52a232
simtape <sim1cassetta@gmail.com>
2022-06-10 19:42:33 +0200

    feat: added suppliers page

---
+++ src/app/suppliers/suppliers.page.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { SupplierService } from '../services/supplier.service';
+
+@Component({
+  selector: 'app-suppliers',
+  templateUrl: './suppliers.page.html',
+  styleUrls: ['./suppliers.page.scss'],
+})
+export class SuppliersPage implements OnInit {
+
+  suppliers: any
+
+
+  constructor(private supplierService: SupplierService,
+        private menu: MenuController) { }
+
+  ngOnInit() {
+    this.getSuppliers();
+  }
+
+  getSuppliers() {
+    this.supplierService.getAllSuppliers().then((res) => {
+      if (res) {
+        this.suppliers = res;
+        console.log(JSON.stringify(this.suppliers))
+      }
+    })
+  }
+
+  openMenu() {
+    this.menu.open();
+  }
+
+
+  ionViewWillEnter() {
+    this.menu.enable(true);
+  }
+
+
+}
